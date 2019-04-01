04e6e162b19a922ba575fdeb57bbb7e08bfaccc6
erclu <30255227+erclu@users.noreply.github.com>
2019-04-01 14:04:59 +0200

    finish setting up model

--- solver/model.py
+++ solver/model.py
@@ -1,4 +1,11 @@
-from typing import Set, List
+from typing import List, Set
+
+
+def score_tags(first: Set[str], second: Set[str]) -> int:
+    common_tags = len(first.intersection(second))
+    first_diff = len(first) - common_tags
+    second_diff = len(second) - common_tags
+    return min(common_tags, first_diff, second_diff)
 
 
 class Photo:
@@ -26,7 +33,17 @@ class Photo:
         return cls(photo_id, orientation, tags)
 
 
-class HorizontalSlide:
+class Slide:
+
+    @property
+    def tags(self) -> Set[str]:
+        pass
+
+    def score(self, other) -> int:
+        return score_tags(self.tags, other.tags)
+
+
+class HorizontalSlide(Slide):
 
     def __init__(self, photo):
         assert photo.orientation == "H", (
@@ -35,18 +52,14 @@ class HorizontalSlide:
         self.photo = photo
 
     def __repr__(self):
-        return self.photo.photo_id
+        return str(self.photo.photo_id)
 
     @property
     def tags(self) -> Set[str]:
         return self.photo.tags
 
-    def score(self, other) -> int:
-        #TODO scoring function
-        pass
 
-
-class VerticalSlide:
+class VerticalSlide(Slide):
 
     def __init__(self, first_photo: Photo, second_photo: Photo):
         assert (
@@ -61,3 +74,35 @@ class VerticalSlide:
     @property
     def tags(self) -> Set[str]:
         return self.first.tags.union(self.second.tags)
+
+
+class Slideshow:
+
+    def __init__(self):
+        self.slides: List[Slide] = []
+        self._score = 0
+
+    def __repr__(self):
+        return str(self.slides)
+
+    def to_string(self) -> str:
+        return f"{len(self.slides)}\n" + "\n".join(
+          str(x) for x in self.slides
+        ) + "\n"
+
+    def append(self, slide: Slide) -> None:
+        if self.slides:
+            self._score += self.slides[-1].score(slide)
+        self.slides.append(slide)
+
+    def score(self) -> int:
+        return self._score
+
+    @classmethod
+    def from_list(cls, slides_list):
+        slideshow = cls()
+
+        for slide in slides_list:
+            slideshow.append(slide)
+
+        return slideshow
