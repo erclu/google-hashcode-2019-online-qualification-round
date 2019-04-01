eea979b5e081123fd67bd52a9c58c015f9796e0d
erclu <30255227+erclu@users.noreply.github.com>
2019-04-01 21:05:11 +0200

    update .style.yapf and fix package entry point

--- solver/__main__.py
+++ solver/__main__.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == "__main__":
     from solver.solve import do_one, do_all
 
-    if sys.argv[1]:
+    if len(sys.argv) == 2:
         do_one(sys.argv[1])
     else:
         do_all()
