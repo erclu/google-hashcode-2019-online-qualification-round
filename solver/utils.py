50a2e76f771a38758fb537ffea0e2ecc4d336960
erclu <30255227+erclu@users.noreply.github.com>
2019-04-01 14:05:45 +0200

    add utility functions and tests for model

--- solver/utils.py
+++ solver/utils.py
@@ -1,7 +1,16 @@
 from pathlib import Path
 from typing import List
 
+from . import model
 
-def read_photos_file(filename: Path) -> List[str]:
-    lines = filename.read_text().split("\n")
-    return lines[1:-1]
+
+def photos_from_file(filename: Path) -> List[model.Photo]:
+    content = filename.read_text()
+    # skip first and last line in file
+    photos_lines: List[str] = content.split("\n")[1:-1]
+
+    return [model.Photo.from_str(pid, x) for pid, x in enumerate(photos_lines)]
+
+
+def write_output_file(filename: Path, slideshow: model.Slideshow) -> None:
+    filename.write_text(slideshow.to_string(), encoding="UTF-8")
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,8 @@
+from itertools import permutations
 from pathlib import Path
 
-import solver.utils
-from solver.model import Photo
+from solver import model
+from solver.utils import photos_from_file, write_output_file
 
 
 def test_example_file():
@@ -9,5 +10,82 @@ def test_example_file():
                               ).resolve().parents[1]/"in"/"a_example.txt"
     assert example_file.exists()
 
-    lines = solver.utils.read_photos_file(example_file)
-    photos = [Photo.from_str(pid, x) for pid, x in enumerate(lines)]
+    photos = photos_from_file(example_file)
+
+    hor_slide = model.HorizontalSlide(photos[0])
+    another_hor_slide = model.HorizontalSlide(photos[3])
+    vert_slide = model.VerticalSlide(photos[1], photos[2])
+
+    assert all(tag in hor_slide.tags for tag in ["cat", "beach", "sun"])
+    assert another_hor_slide.photo.orientation == "H"
+    assert vert_slide.tags == {"selfie", "smile", "garden"}
+
+    slides_list = [hor_slide, another_hor_slide, vert_slide]
+
+    results = [2, 1, 1, 1, 1, 2]
+
+    for perm, result in zip(permutations(slides_list), results):
+        slideshow = model.Slideshow.from_list(perm)
+        assert slideshow.score() == result
+
+
+def test_write():
+    example_file: Path = Path(__file__
+                              ).resolve().parents[1]/"in"/"a_example.txt"
+    assert example_file.exists()
+
+    photos = photos_from_file(example_file)
+
+    slides_list = [
+      model.HorizontalSlide(photos[0]),
+      model.HorizontalSlide(photos[3]),
+      model.VerticalSlide(photos[1], photos[2])
+    ]
+    slideshow = model.Slideshow.from_list(slides_list)
+
+    example_out_file: Path = Path(__file__).resolve(
+    ).parents[1]/"out"/"output_of_example.txt"
+
+    write_output_file(example_out_file, slideshow.to_string())
+
+    #TODO improve this test (fixtures?)
+
+
+def test_slideshow_score():
+    scores = [21081, 1416, 412436, 361153]
+    _parent_folder = Path(__file__).resolve().parents[1]
+
+    for x in range(1, 5):
+        input_file: Path = _parent_folder.joinpath("in", f"input{x}.txt")
+        solution: Path = _parent_folder.joinpath(
+          "out_submitted", f"output{x}.txt"
+        )
+        assert input_file.exists() and solution.exists()
+
+        photos = photos_from_file(input_file)
+
+        sol_indexes = solution.read_text().split("\n")[1:-1]
+
+        slideshow = model.Slideshow()
+
+        for str_index in sol_indexes:
+            if " " not in str_index:
+                index = int(str_index)
+                photo: model.Photo = photos[index]
+                assert photo.photo_id == index
+                slide: model.HorizontalSlide = model.HorizontalSlide(photo)
+            else:
+                split_str_index = str_index.split(" ")
+                i, j = int(split_str_index[0]), int(split_str_index[1])
+                first_photo: model.Photo = photos[i]
+                second_photo: model.Photo = photos[j]
+                assert first_photo.photo_id == i and second_photo.photo_id == j
+                slide: model.VerticalSlide = model.VerticalSlide(
+                  first_photo, second_photo
+                )
+
+            slideshow.append(slide)
+
+        assert slideshow.score() == scores[x - 1]
+
+    # assert False
