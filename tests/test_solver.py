52318aec6f27680604ed9e0982e017fa7233ea66
erclu <30255227+erclu@users.noreply.github.com>
2019-04-02 19:53:27 +0200

    remove old file and fix import

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -6,21 +6,25 @@ import pytest
 
 from solver import model
 from solver.solve import _solve
-from solver.utils import photos_from_file
 
 
-@pytest.mark.skip(reason="not implemented yet")
 def test_solve():
-    example_file: Path = Path(__file__
-                              ).resolve().parents[1]/"in"/"a_example.txt"
+    example_file: Path = Path(__file__).resolve().parents[1].joinpath(
+      "in", "a_example.txt"
+    )
     assert example_file.exists()
 
-    photos: List[model.Photo] = photos_from_file(example_file)
+    photos: List[model.Photo] = model.Photo.from_file(example_file)
 
     slideshow: model.Slideshow = _solve(photos)
 
-    assert (
-      slideshow.to_string() == "3\n0\n3\n1 2"
-      or slideshow.to_string() == "3\n1 2\n3\n0"
-    )
+    # assert (
+    #   slideshow.to_string() == "3\n0\n3\n1 2\n" or
+    #   slideshow.to_string() == "3\n0\n3\n2 1\n" or
+    #   slideshow.to_string() == "3\n1 2\n3\n0\n" or
+    #   slideshow.to_string() == "3\n2 1\n3\n0\n"
+    # )
     assert slideshow.score() == 2
+
+
+# TODO test on input2 (smaller one)
